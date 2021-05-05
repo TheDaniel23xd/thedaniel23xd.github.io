22dab59106f8ceafe797fede8714acab3467884e
TheDaniel23xd <danielcarcamomartinez@gmail.com>
2021-05-05 15:15:41 -0600

    Glassmorphism_Battery_Level

---
+++ Glassmorphism_Battery_Level/README.md
@@ -0,0 +1,6 @@
+### Proyectos
+
+| ------------ | ------------ | ------------ |
+
+<img align="right" alt="Img" src="./images/Glassmorphism_BatteryLevel.png" />
+<img align="right" alt="Img" src="./images/Glassmorphism_Battery_Level.png" />
---
+++ Glassmorphism_Battery_Level/js/app.js
@@ -0,0 +1,14 @@
+let percentage = document.querySelector('.percentage');
+let percent = document.querySelector('.percent');
+
+navigator.getBattery().then(function(battery){
+    percentage.style.width = battery.level * 100 + '%';
+    percent.innerHTML = battery.level * 100 + '%';
+})
+
+let sec = document.querySelector('.sec');
+let toggle = document.querySelector('.toggle');
+
+toggle.onclick = function(){
+    sec.classList.toggle('dark');
+}
